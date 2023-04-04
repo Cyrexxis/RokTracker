d9a264fb400acb452ece21bec79337547009803f
Cyrexxis <cyrexxis@outlook.com>
2023-04-04 14:27:19 +0200

    added python version check to alliance and rok scanner (closes #10) and checking powershell host in the ps1 file

--- rok-scanner.ps1
+++ rok-scanner.ps1
@@ -1,3 +1,6 @@
+echo "Checking powershell host: "
+if(($host.Name -match 'consolehost')) {echo "OK ($($host.Name))"} else { echo "Expected ConsoleHost but got $($host.Name). Script might fail to execute. For correct execution use the normal powershell not the ISE."}
+
 python -m venv ./venv
 invoke-expression -Command .\venv\Scripts\Activate.ps1
 pip install -r requirements.txt
