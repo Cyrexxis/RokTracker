3731aedb90333120b47acdc3297c256a1269ae3f
Cyrexxis <cyrexxis@outlook.com>
2023-08-10 14:19:11 +0200

    updated roi and click positions. fixes #18

--- rok_ui_positions.py
+++ rok_ui_positions.py
@@ -1,23 +1,23 @@
 # format: (x, y, width, height)
 ocr_regions = {
     # first screen
-    "more_info": (313, 727, 137, 29),
-    "gov_id": (774, 230, 260, 38),
-    "power": (890, 364, 170, 44),
-    "killpoints": (1114, 364, 222, 44),
-    "alliance_name": (645, 362, 260, 40),
+    "more_info": (285, 786, 137, 29),
+    "gov_id": (732, 191, 260, 38),
+    "power": (885, 326, 196, 38),
+    "killpoints": (1135, 326, 216, 38),
+    "alliance_name": (602, 326, 279, 38),
     # second screen
-    "t1_kills": (862, 461, 150, 38),
-    "t2_kills": (862, 505, 150, 38),
-    "t3_kills": (862, 549, 150, 38),
-    "t4_kills": (862, 593, 150, 38),
-    "t5_kills": (862, 638, 150, 38),
-    "t1_killpoints": (1243, 461, 171, 38),
-    "t2_killpoints": (1243, 505, 171, 38),
-    "t3_killpoints": (1243, 549, 171, 38),
-    "t4_killpoints": (1243, 593, 171, 38),
-    "t5_killpoints": (1243, 638, 171, 38),
-    "ranged_points": (1274, 740, 146, 38),
+    "t1_kills": (862, 423, 200, 38),
+    "t2_kills": (862, 467, 200, 38),
+    "t3_kills": (862, 511, 200, 38),
+    "t4_kills": (862, 555, 200, 38),
+    "t5_kills": (862, 600, 200, 38),
+    "t1_killpoints": (1243, 423, 171, 38),
+    "t2_killpoints": (1243, 467, 171, 38),
+    "t3_killpoints": (1243, 511, 171, 38),
+    "t4_killpoints": (1243, 555, 171, 38),
+    "t5_killpoints": (1243, 600, 171, 38),
+    "ranged_points": (1220, 702, 200, 38),
     # third screen
     "deads": (1130, 443, 183, 40),
     "rss_gathered": (1130, 612, 183, 40),
@@ -28,9 +28,9 @@ ocr_regions = {
 # format: (x, y)
 tap_positions = {
     # first screen
-    "name_copy": (690, 283),
-    "open_kills": (1118, 350),
-    "more_info": (387, 664),
+    "name_copy": (626, 249),
+    "open_kills": (1119, 314),
+    "more_info": (356, 739),
     "close_gov": (1365, 104),
     # third screen
     "close_info": (1396, 58),
