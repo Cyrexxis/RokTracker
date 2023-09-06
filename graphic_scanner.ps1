f776086c01089a4182a86a7dea513e4807c631ec
Cyrexxis <cyrexxis@outlook.com>
2023-09-06 12:16:54 +0200

    added bat file to start the scanner with double click and added some checks to catch missing folders on start

---
+++ graphic_scanner.ps1
@@ -0,0 +1,7 @@
+echo "Checking powershell host: "
+if(($host.Name -match 'consolehost')) {echo "OK ($($host.Name))"} else { echo "Expected ConsoleHost but got $($host.Name). Script might fail to execute. For correct execution use the normal powershell not the ISE."}
+
+python -m venv ./venv
+invoke-expression -Command .\venv\Scripts\activate
+pip install -r requirements.txt
+python .\graphic_scanner.py
---
+++ run_scanner_ui.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+SET "scriptdir=%~dp0"
+IF NOT "%scriptdir:~-1%"=="\" SET "scriptdir=%scriptdir%\"
+ECHO Installation folder: %scriptdir%
+
+IF NOT EXIST "%scriptdir%platform-tools\" (
+  ECHO The platform-tools folder is missing. Please follow the installation process in the readme.
+  ECHO It should be located at "%scriptdir%platform-tools\"
+  PAUSE
+  EXIT
+)
+
+IF NOT EXIST "%scriptdir%deps\" (
+  ECHO The deps folder is missing. Please follow the installation process in the readme.
+  ECHO It should be located at "%scriptdir%deps\"
+  PAUSE
+  EXIT
+)
+
+IF NOT EXIST "%scriptdir%deps\tessdata-main\" (
+  ECHO The tessdata-main folder is missing in the deps folder. Please follow the installation process in the readme.
+  ECHO It should be located at "%scriptdir%deps\tessdata-main\"
+  PAUSE
+  EXIT
+)
+
+powershell.exe -noLogo -ExecutionPolicy unrestricted -file graphic_scanner.ps1
