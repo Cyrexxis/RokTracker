8a5c01d0c64cf66cc1f0fe1b8e329487b6734504
Cyrexxis <cyrexxis@outlook.com>
2023-09-13 22:17:56 +0200

    added installation skripts

---
+++ install_scanner.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+powershell.exe -noLogo -ExecutionPolicy unrestricted -file %scriptdir%install_scanner.ps1
+PAUSE
---
+++ install_scanner.ps1
@@ -0,0 +1,19 @@
+echo "Checking powershell host: "
+if(($host.Name -match 'consolehost')) {echo "OK ($($host.Name))"} else { echo "Expected ConsoleHost but got $($host.Name). Script might fail to execute. For correct execution use the normal powershell not the ISE."}
+
+python .\check_python.py
+if($LastExitCode -eq 1){
+    Exit
+}
+
+python -m venv ./venv
+invoke-expression -Command .\venv\Scripts\activate
+pip install tqdm==4.66.1
+
+python .\install_scanner.py
+if($LastExitCode -eq 1){
+    Exit
+}
+
+pip install -r requirements.txt
+pip install .\deps\tesserocr-2.6.0-cp311-cp311-win_amd64.whl
