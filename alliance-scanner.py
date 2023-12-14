38826e08448503eafa189009579670a194296420
Cyrexxis <cyrexxis@outlook.com>
2023-12-14 23:24:40 +0100

    removed old code and added duplicate detection to alliance/honor scanner

--- alliance-scanner.py
+++ alliance-scanner.py
@@ -13,19 +13,6 @@ if current_version < required_version:
     )
     sys.exit(1)
 
-import pytesseract
-
-# pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
-try:
-    tes_ver = pytesseract.get_tesseract_version()
-except:
-    print(
-        "Could not find tesseract, please set the installation path manually in alliance-scanner.py (uncomment line 12 and set path to your executable)."
-    )
-    sys.exit(1)
-else:
-    print(f"Found tesseract version: {tes_ver}")
-
 from console import console
 from adbutils import *
 from rich.console import Console
@@ -259,14 +246,43 @@ def print_results(
     console.print(table)
 
 
+def check_for_duplicate(governor, sheet, governor_number):
+    for row in sheet.iter_rows(
+        min_col=2,
+        max_col=3,
+        min_row=max(2, governor_number - 5),
+        max_row=governor_number + 1,
+    ):
+        if row[0].value == governor["name"] and row[1].value == to_int_check(
+            governor["score"]
+        ):
+            return True
+    return False
+
+
 def add_results_to_sheet(governors, sheet, screen_number):
+    global reached_bottom
     govs_per_screen = len(governors)
+    duplicates = 0
     for gov_num, governor in enumerate(governors):
-        current_gov = (govs_per_screen * screen_number) + gov_num
+        current_gov = (govs_per_screen * screen_number) + gov_num - duplicates
+
+        if check_for_duplicate(governor, sheet, current_gov):
+            print(f"Removed duplicated governor ({governor['name']}).")
+            duplicates += 1
+            # Duplicate can only happen on the last page
+            reached_bottom = True
+            continue
+
         sheet.row_dimensions[current_gov + 2].height = 24.75
         # Write results in excel file
         sheet.add_image(
-            OpImage(str(img_path / ("gov_name_" + str(current_gov + 1) + ".png"))),
+            OpImage(
+                str(
+                    img_path
+                    / ("gov_name_" + str(current_gov + 1 + duplicates) + ".png")
+                )
+            ),
             "A" + str(current_gov + 2),
         )
         sheet["B" + str(current_gov + 2)] = governor["name"]
@@ -531,85 +547,6 @@ def scan_screen(mode: str, port: int, screen_number: int):
         return []
 
 
-def governor_scan(mode: str, port: int, gov_number: int):
-    # set up the scan variables
-    global scan_index
-    global reached_bottom
-    gov_name = ""
-    gov_score = 0
-    tries = 0
-    max_tries = 3
-
-    if reached_bottom:
-        scan_index = scan_index + 1
-        if scan_index >= len(mode_data[mode]["name_pos"]):
-            console.log("Last governor scanned, scan completed.")
-            exit(0)
-
-    # Take screenshot to process
-    secure_adb_screencap(port).save("currentState.png")
-    image = cv2.imread("currentState.png")
-
-    # Power and Killpoints
-    while tries < max_tries:
-        # needed for detection in alliance mode
-        try:
-            im_gov_name = cropToRegion(image, mode_data[mode]["name_pos"][scan_index])
-            im_gov_name_bw = preprocessImage(
-                im_gov_name,
-                3,
-                mode_data[mode]["threshold"],
-                12,
-                mode_data[mode]["invert"],
-            )
-            im_gov_name_bw_small = preprocessImage(
-                im_gov_name,
-                1,
-                mode_data[mode]["threshold"],
-                4,
-                mode_data[mode]["invert"],
-            )
-            cv2.imwrite("tmp/gov_name" + str(gov_number) + ".png", im_gov_name_bw_small)
-
-            image = cv2.imread("currentState.png")
-            im_gov_score = cropToRegion(image, mode_data[mode]["score_pos"][scan_index])
-            im_gov_score_bw = preprocessImage(
-                im_gov_score,
-                3,
-                mode_data[mode]["threshold"],
-                12,
-                mode_data[mode]["invert"],
-            )
-
-            # gov_name = pytesseract.image_to_string(im_gov_name,config="--oem 1 -l ara+chi_sim+chi_tra+deu+ell+grc+hat+hrv+heb+hin+ind+jpn+kor+lao+mal+mon+rus+san+swa+tha+tur+vie")
-            gov_name = pytesseract.image_to_string(
-                im_gov_name_bw, config="--oem 1 --psm 7 -l eng"
-            )
-            gov_score = pytesseract.image_to_string(
-                im_gov_score_bw, config="--oem 1 --psm 8"
-            )
-
-            gov_score = int(re.sub("[^0-9]", "", gov_score))
-            break
-        except:
-            if not reached_bottom:
-                tries = tries + 1
-                scan_index = scan_index + 1
-                reached_bottom = True
-                console.log(
-                    "Didn't find score, assuming bottom is reached. Starting scan of last 6 governors."
-                )
-            else:
-                console.log("Aborting scan, because an error occurred.")
-                exit(0)
-
-    # Just to check the progress, printing in cmd the result for each governor
-    if gov_score == "":
-        gov_score = "Unknown"
-
-    return {"name": gov_name, "score": gov_score}
-
-
 def fast_scan(port: int, kingdom: str, mode: str, amount: int):
     govs_per_screen = 1
 
@@ -715,119 +652,6 @@ def fast_scan(port: int, kingdom: str, mode: str, amount: int):
     for p in img_path.glob("gov_name*.png"):
         p.unlink()
 
-    return
-
-
-def scan(port: int, kingdom: str, mode: str, amount: int, resume: bool):
-    # Initialize the connection to adb
-    device = start_adb(port)
-
-    ######Excel Formatting
-    wb = Workbook()
-    sheet1 = wb.active
-    sheet1.title = str(datetime.date.today())
-
-    # Make Head Bold
-    font = Font(bold=True)
-
-    # Initialize Excel Sheet Header
-    sheet1["A1"] = "Name Image"
-    sheet1["B1"] = "Governor Name"
-    sheet1["C1"] = "Governor Score"
-
-    sheet1.column_dimensions["A"].width = 42
-    sheet1["A1"].font = font
-    sheet1["B1"].font = font
-    sheet1["C1"].font = font
-
-    stop = False
-
-    Path("./tmp").mkdir(parents=True, exist_ok=True)
-
-    for i in range(0, amount):
-        if abort_scan:
-            console.print("Scan Terminated! Saving the current progress...")
-            break
-
-        governor = governor_scan(mode, port, i)
-
-        # needed for detection of end in honor mode
-        if sheet1["B" + str(i + 1)].value == governor["name"] and sheet1[
-            "C" + str(i + 1)
-        ].value == to_int_check(governor["score"]):
-            global reached_bottom
-            reached_bottom = True
-            console.log("Duplicate governor, switching to scan of last governors")
-            governor = governor_scan(mode, port, i)
-
-        now = datetime.datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-
-        # nice output for console
-        table = Table(
-            title="["
-            + current_time
-            + "]\n"
-            + "Latest Scan Result\nGovernor "
-            + str(i + 1)
-            + " of "
-            + str(amount),
-            show_header=True,
-            show_footer=True,
-        )
-        table.add_column("Entry", "Approx time remaining\nSkipped", style="magenta")
-        table.add_column(
-            "Value", str(datetime.timedelta(seconds=(amount - i) * 6)), style="cyan"
-        )
-
-        table.add_row("Governor Name", governor["name"])
-        table.add_row("Governor Score", str(governor["score"]))
-
-        console.print(table)
-
-        sheet1.row_dimensions[i + 2].height = 24.75
-        # Write results in excel file
-        image = OpImage("tmp/gov_name" + str(i) + ".png")
-        sheet1.add_image(OpImage("tmp/gov_name" + str(i) + ".png"), "A" + str(i + 2))
-        sheet1["B" + str(i + 2)] = governor["name"]
-        sheet1["C" + str(i + 2)] = to_int_check(governor["score"])
-
-        if resume:
-            file_name_prefix = "NEXT"
-        else:
-            file_name_prefix = "TOP"
-        wb.save(
-            file_name_prefix
-            + str(amount)
-            + "-"
-            + str(start_date)
-            + "-"
-            + kingdom
-            + f"-[{run_id}]"
-            + ".xlsx"
-        )
-
-        if reached_bottom:
-            break
-
-        adb_send_events("Touch", mode_data[mode]["script"], port)
-
-        time.sleep(1 + random.random())
-    if resume:
-        file_name_prefix = "NEXT"
-    else:
-        file_name_prefix = "TOP"
-    wb.save(
-        file_name_prefix
-        + str(amount)
-        + "-"
-        + str(start_date)
-        + "-"
-        + kingdom
-        + f"-[{run_id}]"
-        + ".xlsx"
-    )
-    kill_adb()  # make sure to clean up adb server
     return
 
 
